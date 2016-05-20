56dff0fdfc6951155c9d283c8de431ae9a3fee64
Chanpory Rith <chanpory@mixmax.com>
2016-05-20 10:48:39 -0700

    Calculate iframe height

--- script_iframe.js
+++ script_iframe.js
@@ -3,13 +3,21 @@ $(function() {
     $('.js-editor-body').scrollTop(offset);
   };
 
-  var setEditorWidth = function(width){
-    $('.js-editor-body').width(width);
+  var setIframeHeight = function() {
+    var headerHeight = $('.js-header').outerHeight();
+    var editorHeight = $('.js-editor-body').outerHeight() + 16; // Remove this magic number. It's the padding around the editor.
+    var toolbarHeight = $('.js-footer').outerHeight();
+    var height = headerHeight + editorHeight + toolbarHeight;
 
     parent.postMessage({
       message: 'setIframeHeight',
-      iframeHeight: 500
+      iframeHeight: height
     }, '*');
+  }
+
+  var setEditorWidth = function(width){
+    $('.js-editor-body').width(width);
+    setIframeHeight();
   };
 
   var handlePostMessage = function(e) {
@@ -18,10 +26,11 @@ $(function() {
     }
 
     if (e.data.message == 'setEditorWidth') {
-      console.log(e);
       setEditorWidth(e.data.editorWidth);
     }
   }
+
+  setIframeHeight();
 
   window.addEventListener("message", handlePostMessage, false);
 });
