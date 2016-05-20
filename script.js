6d3bf1e80de731737cc1e40c82cfeddc1f19955e
Chanpory Rith <chanpory@mixmax.com>
2016-05-20 02:20:29 -0700

    Clean up

--- script.js
+++ script.js
@@ -2,9 +2,7 @@ $(function() {
     var $mixmaxReply = $('.js-mixmax-reply-box');
     var $oldReply = $('.js-old-reply-parent');
     var $mixmaxEditorIframe = $('.js-mixmax-editor-iframe');
-
     var oldReplyBounds = $oldReply[0].getBoundingClientRect();
-
     var marginBottomOffset = 10; // When pinned at the bottom, the desired space between bottom of iFrame and bottom of window.
     var marginTopOffset = 10; // When pinned at the top, the desired space between top of iFrame and top of window.
     var naturalIframeHeight = 700; // The natural iframe height which would show all content of the editor without scrolling.
@@ -12,14 +10,10 @@ $(function() {
 
     var updatePosition = function() {
       var docHeight = document.documentElement.clientHeight;
-
       var oldReplyBounds = $oldReply[0].getBoundingClientRect();
       var leftOffset = oldReplyBounds.left;
       var distanceFromBottom = Math.max(docHeight - oldReplyBounds.bottom - marginBottomOffset, 0);
-
-      var viewPortHeight = docHeight - marginTopOffset - marginBottomOffset - distanceFromBottom;
-      var maxIframeHeight = Math.min(viewPortHeight, naturalIframeHeight);
-
+      var maxIframeHeight = Math.min(docHeight - marginTopOffset - marginBottomOffset - distanceFromBottom, naturalIframeHeight);
       var newHeight = Math.min(docHeight - oldReplyBounds.top - marginBottomOffset , maxIframeHeight);
       var topOffset = newHeight < minIframeHeight && oldReplyBounds.top < marginTopOffset ? -(minIframeHeight - oldReplyBounds.bottom) : Math.max(oldReplyBounds.top, marginTopOffset);
 
